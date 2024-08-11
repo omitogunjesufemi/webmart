808920140fad27b21bc4f6110e278310fe52e4fa
Jesufemi Omitogun <omitogunjesufemi@gmail.com>
2024-08-11 23:01:34 +0100

    feat: Add Category, Order, and User routes and controllers
    This commit adds the CategoryRoute, OrderRoute, and UserRoute modules to the routes directory. These modules define the routes for managing categories, orders, and users. Additionally, the commit adds the CategoryController, OrderController, and UserController classes to their respective files in the controllers directory. These classes contain methods for handling the logic related to categories, orders, and users. These changes are necessary to implement the functionality for managing categories, orders, and users in the webmart project.

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import appRouter from './routes/AppRoute'
 import productRouter from './routes/ProductRoute';
+import categoryRouter from './routes/CategoryRoute';
+import orderRouter from './routes/OrderRoute';
+import userRouter from './routes/UserRoute';
 
 const app = express();
 
@@ -10,6 +13,9 @@ const PORT = process.env.PORT || 3000;
 
 app.use('/api/v1/', appRouter);
 app.use('/api/v1/', productRouter);
+app.use('/api/v1/', categoryRouter);
+app.use('/api/v1/', orderRouter);
+app.use('/api/v1/', userRouter);
 
 app.listen(PORT, () => {
   console.log('Server is running on port: ', PORT);
