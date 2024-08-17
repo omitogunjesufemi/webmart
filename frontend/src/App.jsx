22516088eebcfad8a8f17a94b733fb8d6c25e349
Jesufemi Omitogun <omitogunjesufemi@gmail.com>
2024-08-17 12:13:59 +0100

    feat: Add react-icons dependency and implement search functionality in Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom"
 import MainLayout from "./layouts/MainLayout";
 import HomePage from "./pages/HomePage";
+import CategoriesPage from "./pages/products/CategoriesPage";
 
 export default function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<MainLayout />}>
         <Route index element={<HomePage />} />
+        <Route path="/categories" element={<CategoriesPage />} />
       </Route>
     )
   );
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import { NavLink } from "react-router-dom";
+import { FaCartShopping } from 'react-icons/fa6';
 
 export default function Navbar() {
   return (
@@ -9,17 +10,22 @@ export default function Navbar() {
           <div
             className="flex flex-1 items-center justify-center md:items-stretch md:justify-start"
           >
+            <div className="flex flex-1">
+                <input type="text" placeholder="Search for products..." className="w-full max-w-xs bg-gray-100 text-gray-700 px-4 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" />
+            </div>
             {/* <!-- Logo --> */}
-            <NavLink className="flex flex-shrink-0 items-center mr-4" to="/">
-              <img
+
+            <NavLink className="flex justify-center flex-shrink-0 items-center mr-4" to="/">
+              {/* <img
                 className="h-10 w-auto"
                 src=""
                 alt="web mart"
-              />
+              /> */}
               <span className="hidden md:block text-white text-2xl font-bold ml-2"
                 >webmart</span
               >
             </NavLink>
+            
             <div className="md:ml-auto">
               <div className="flex space-x-2">
                 <NavLink
@@ -35,7 +41,8 @@ export default function Navbar() {
                 <NavLink
                   to="/add-job"
                   className='text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'
-                  >Cart
+                  >
+                  <FaCartShopping /> 
                 </NavLink>
               </div>
             </div>
