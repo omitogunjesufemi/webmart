7c386ac80abdf5902187c0d3853f2b43bfe55de6
Jesufemi Omitogun <omitogunjesufemi@gmail.com>
2024-08-20 11:56:57 +0100

    feat: Add lazy loading for images and update dependencies
    This commit adds lazy loading functionality for images in the webmart application, improving page load performance. It also updates the dependencies in the package.json file, adding the "flowbite" and "flowbite-react" packages. These changes enhance the overall performance and usability of the webmart application.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,13 @@ import HomePage from "./pages/HomePage";
 import CategoriesPage from "./pages/products/CategoriesPage";
 import Register from "./pages/account/Register";
 import Login from "./pages/account/Login";
+import ProductDetail from "./pages/products/ProductDetail";
+import ProductsListing from "./components/ProductsListing";
+import OrderSummary from "./pages/orders/OrderSummary";
+import ShoppingCart from "./pages/orders/ShoppingCart";
+import CheckoutPage from "./pages/orders/CheckoutPage";
+import ErrorPage from "./pages/error/ErrorPage";
+import OrdersOverview from "./pages/orders/OrdersOverview";
 
 export default function App() {
   const router = createBrowserRouter(
@@ -11,8 +18,19 @@ export default function App() {
       <Route path="/" element={<MainLayout />}>
         <Route index element={<HomePage />} />
         <Route path="/categories" element={<CategoriesPage />} />
+
+        <Route path="/products/:id" element={<ProductDetail />} />
+        <Route path="/products" element={<ProductsListing />} />
+
+        <Route path="/carts/:id" element={<ShoppingCart />} />
+        <Route path="/checkouts/:id" element={<CheckoutPage />} />
+        <Route path="/orders/:id" element={<OrderSummary />} />
+        <Route path="/orders" element={<OrdersOverview />} />
+
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+
+        <Route path="*" element={<ErrorPage />} />
       </Route>
     )
   );
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,34 +1,60 @@
 /** @type {import('tailwindcss').Config} */
-import daisyui from 'daisyui';
+
+import flowbiteplugin from "flowbite/plugin";
 
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    'node_modules/flowbite-react/lib/esm/**/*.js'
   ],
+
+  dark: 'class',
   theme: {
     extend: {
-      fontFamily: {
-        sans: ['Roboto', 'sans-serif']
+      colors: {
+        primary: {"50":"#eff6ff","100":"#dbeafe","200":"#bfdbfe","300":"#93c5fd","400":"#60a5fa","500":"#3b82f6","600":"#2563eb","700":"#1d4ed8","800":"#1e40af","900":"#1e3a8a","950":"#172554"}
       },
-
-      gridTemplateColumns: {
-        '70/30': '70% 28%'
+      fontFamily: {
+      'body': [
+      'Inter', 
+      'ui-sans-serif', 
+      'system-ui', 
+      '-apple-system', 
+      'system-ui', 
+      'Segoe UI', 
+      'Roboto', 
+      'Helvetica Neue', 
+      'Arial', 
+      'Noto Sans', 
+      'sans-serif', 
+      'Apple Color Emoji', 
+      'Segoe UI Emoji', 
+      'Segoe UI Symbol', 
+      'Noto Color Emoji'
+    ],
+      'sans': [
+      'Inter', 
+      'ui-sans-serif', 
+      'system-ui', 
+      '-apple-system', 
+      'system-ui', 
+      'Segoe UI', 
+      'Roboto', 
+      'Helvetica Neue', 
+      'Arial', 
+      'Noto Sans', 
+      'sans-serif', 
+      'Apple Color Emoji', 
+      'Segoe UI Emoji', 
+      'Segoe UI Symbol', 
+      'Noto Color Emoji'
+    ]
       }
     },
-  },
+  
   plugins: [
-    daisyui,
+    flowbiteplugin,    
   ],
-
-  daisyui: {
-    themes: false, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
-    darkTheme: "dark", // name of one of the included themes for dark mode
-    base: true, // applies background color and foreground color for root element by default
-    styled: true, // include daisyUI colors and design decisions for all components
-    utils: true, // adds responsive and modifier utility classes
-    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
-    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
-    themeRoot: ":root", // The element that receives theme color CSS variables
-  },
+  }
 }
