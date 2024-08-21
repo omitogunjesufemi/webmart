4297e1b12f96cc4dc1d8cd20c6d318416c1f745a
Jesufemi Omitogun <omitogunjesufemi@gmail.com>
2024-08-22 00:33:28 +0100

    feat: Add isLoggedIn state and update Navbar component
    This commit adds the isLoggedIn state to the Navbar component in order to track the user's login status. It also updates the component's rendering logic to display different navigation links based on the user's login status. These changes improve the user experience by providing personalized navigation options and ensuring that the correct links are displayed for logged-in and logged-out users.

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,10 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 export default function Navbar() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   useEffect(() => {
-  }, [])  
+    setIsLoggedIn(document.cookie.split(';').some((item) => item.trim().startsWith('USR_EMAIL')));
+  }, []);
 
   return (
     <nav className="bg-white dark:bg-gray-800 antialiased">
@@ -50,7 +52,7 @@ export default function Navbar() {
           </div>
 
           <div className="flex items-center lg:space-x-2">
-
+            { isLoggedIn ? (<>
             <button id="myCartDropdownButton1" data-dropdown-toggle="myCartDropdown1" type="button" className="inline-flex items-center rounded-lg justify-center p-2 hover:bg-gray-100 dark:hover:bg-gray-700 text-sm font-medium leading-none text-gray-900 dark:text-white">
               <span className="sr-only">
                 Cart
@@ -202,6 +204,41 @@ export default function Navbar() {
                 <a href="#" title="" className="inline-flex w-full items-center gap-2 rounded-md px-3 py-2 text-sm hover:bg-gray-100 dark:hover:bg-gray-600"> Sign Out </a>
               </div>
             </div>
+            </>) : 
+            (<>
+             <a href="/carts">
+              <button type="button" className="inline-flex items-center rounded-lg justify-center p-2 hover:bg-gray-100 dark:hover:bg-gray-700 text-sm font-medium leading-none text-gray-900 dark:text-white">
+              <span className="sr-only">
+                Cart
+              </span>
+              <svg className="w-5 h-5 lg:me-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 4h1.5L9 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm-8.5-3h9.25L19 7H7.312"/>
+              </svg> 
+              <span className="hidden sm:flex">Cart</span>           
+              </button>
+            </a>
+
+            <button id="userDropdownButton2" data-dropdown-toggle="userDropdown2" type="button" className="inline-flex items-center rounded-lg justify-center p-2 hover:bg-gray-100 dark:hover:bg-gray-700 text-sm font-medium leading-none text-gray-900 dark:text-white">
+              <svg className="w-5 h-5 me-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                <path stroke="currentColor" strokeWidth="2" d="M7 17v1a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1a3 3 0 0 0-3-3h-4a3 3 0 0 0-3 3Zm8-9a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"/>
+              </svg>              
+              Account
+              <svg className="w-4 h-4 text-gray-900 dark:text-white ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 9-7 7-7-7"/>
+              </svg> 
+            </button>
+
+            <div id="userDropdown2" className="hidden z-10 w-56 divide-y divide-gray-100 overflow-hidden overflow-y-auto rounded-lg bg-white antialiased shadow dark:divide-gray-600 dark:bg-gray-700">
+              <ul className="p-2 text-start text-sm font-medium text-gray-900 dark:text-white">
+                <li><a href="#" title="" className="inline-flex w-full items-center gap-2 rounded-md px-3 py-2 text-sm hover:bg-gray-100 dark:hover:bg-gray-600"> My Account </a></li>
+                <li><a href="#" title="" className="inline-flex w-full items-center gap-2 rounded-md px-3 py-2 text-sm hover:bg-gray-100 dark:hover:bg-gray-600"> My Orders </a></li>
+              </ul>
+          
+              <div className="p-2 text-sm font-medium text-gray-900 dark:text-white hover:text-white hover:bg-primary-600 dark:hover:bg-primary-600">
+                <a href="/login" title="" className="inline-flex w-full items-center gap-2 rounded-md px-3 py-2 text-sm"> Login </a>
+              </div>
+            </div>
+            </>)}
 
             <button type="button" data-collapse-toggle="ecommerce-navbar-menu-1" aria-controls="ecommerce-navbar-menu-1" aria-expanded="false" className="inline-flex lg:hidden items-center justify-center hover:bg-gray-100 rounded-md dark:hover:bg-gray-700 p-2 text-gray-900 dark:text-white">
               <span className="sr-only">
